4117212d9bcaf8c3f23cdc8ea1b4f7d576bc6dc2
CodeAlgorilla <mawei201510@gmail.com>
2018-09-28 12:11:18 +0200

    create frame of Merger

--- MergeDataTool/include/merger.h
+++ MergeDataTool/include/merger.h
@@ -1,1 +1,12 @@
 // This is the head file of csv merger
+#include <fstream>
+#include <vector>
+#include <iterator>
+#include <iostream>
+
+class CsvMerger
+{
+private:
+public:
+
+}
---
+++ MergeDataTool/include/random_number_generator.h
@@ -0,0 +1,1 @@
+// This is a generator of random number, in order to generate random number in a specific range without duplicate.
